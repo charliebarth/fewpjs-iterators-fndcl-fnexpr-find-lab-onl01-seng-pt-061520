af3c1715a2333706b79110f6a1a7e157c4cb3b6f
github username <charliepbarth@gmail.com>
2021-01-15 23:17:45 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  const that_one_year = record.find(won)
+
+  if (that_one_year) {
+    return that_one_year.year
+  } else {
+    return
+  } 
+}
+
+function won(single_record) {
+  return single_record.result === "W"
+}
